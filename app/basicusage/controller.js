9094471dd5a479483b09209f6e6faa80715b1db9
Adrian Lizarraga <adrian.lizarraga@zybooks.com>
2016-10-04 13:56:05 -0700

    Added logic to disable validators if corresponding inputs have not received focus

--- app/basicusage/controller.js
+++ app/basicusage/controller.js
@@ -4,24 +4,29 @@ import { validator, buildValidations } from 'ember-cp-validations';
 /**
    Mixin specifying validations for controller.
 */
-const ValidationsMixin = buildValidations({
-    /**
-       Validator for the title property. Title should not be blank.
-    */
-    title: validator('presence', { presence: true }),
+const ValidationsMixin = buildValidations(
+    {
+        /**
+         Validator for the title property. Title should not be blank.
+         */
+        title: validator('presence', { presence: true, disabled: Ember.computed.not('model.focusedOnInput.title') }),
 
-    /**
-       Validators for the code property. Code should be between 4 and 8 characters.
-    */
-    code: [
-        validator('presence', true),
-        validator('length', {
-            min: 4,
-            max: 8,
-            disabled: Ember.computed.not('model.enableCodeLengthValidator'),
-        }),
-    ],
-});
+        /**
+         Validators for the code property. Code should be between 4 and 8 characters.
+         */
+        code: {
+            disabled: Ember.computed.not('model.focusedOnInput.code'),
+            validators: [
+                validator('presence', true),
+                validator('length', {
+                    min: 4,
+                    max: 8,
+                    dependentKeys: [ 'model.focusedOnInput.code' ],
+                }),
+            ],
+        },
+    },
+);
 
 /**
    The controller for the basic usage page.
@@ -41,5 +46,26 @@ export default Ember.Controller.extend(ValidationsMixin, {
     /**
        True if code length validator should be enabled.
     */
-    enableCodeLengthValidator: true,
+    enableCodeLengthValidator: false,
+
+    /**
+       Keeps track of inputs that have received focus. Maps a property name to a boolean value
+       indicating if the input's focus-out event has been processed.
+    */
+    focusedOnInput: null,
+
+    /**
+       Initializes controller.
+    */
+    init: function() {
+        this._super(...arguments);
+
+        this.set('focusedOnInput', Ember.Object.create({}));
+    },
+
+    actions: {
+        focusOut: function(name) {
+            this.get('focusedOnInput').set(name, true);
+        },
+    },
 });
